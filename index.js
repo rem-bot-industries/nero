5a822e011b2417615f9065ca3490772d5a6ddc12
DasWolke <julianhannig@gmail.com>
2017-02-22 03:36:56 +0100

    >qw<

--- index.js
+++ index.js
@@ -25,7 +25,9 @@ try {
     process.exit(1);
 }
 global.remConfig = config;
+if (remConfig.tracking_enabled) winston.warn(`Tracking is disabled!`);
 winston.info(`Using statsdhost: ${remConfig.dogstatsd_host}`);
+
 let shards = {};
 let StatTrack = require('./statistics/botStatTrack');
 let WsHandler = require('./ws/wsHandler');
